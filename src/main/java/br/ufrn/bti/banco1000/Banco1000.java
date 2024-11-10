ea1336bb2fedf1966e5737a65dc40364931235f3
Gilson Kedson <gilsonkedson15@outlook.com>
2024-11-10 10:20:34 -0300

    LAB1 - Projeto Base

---
+++ src/main/java/br/ufrn/bti/banco1000/Banco1000.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ufrn.bti.banco1000;
+
+/**
+ *
+ * @author vinicius
+ */
+public class Banco1000 {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
